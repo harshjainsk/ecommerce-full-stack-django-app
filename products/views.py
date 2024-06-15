a7302831ab3fd6765b88f6d739784ed9a4460b91
harshjainsk <harshjainsk531@gmail.com>
2024-06-16 00:17:39 +0530

    integrated index and product page with backend

--- products/views.py
+++ products/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 
 
 def get_products(request, slug):
 
+    try:
+        product = Product.objects.get(slug_field = slug)
 
-    return render(request, "product/product.html")
+        return render(request, "product/product.html", context = {"product":product})
+    
+    except Exception as e:
+
+        print(e)
