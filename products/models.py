2c50abd67df0aed08afeaaa8406f936dc7ae6997
harshjainsk <harshjainsk531@gmail.com>
2024-06-15 23:48:48 +0530

    added slug field and changed admin protal view

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from base.models import BaseModel
-
+from django.utils.text import slugify
 
 
 class Category(BaseModel):
@@ -10,6 +10,16 @@ class Category(BaseModel):
     category_image = models.ImageField(upload_to='categories')
 
 
+    def save(self, *args, **kwargs):
+
+        self.slug_field = slugify(self.category_name)
+
+        super(Category,self).save(*args, **kwargs)
+
+    def __str__(self) -> str:
+        return self.category_name
+
+
 class Product(BaseModel):
 
     product_name = models.CharField(max_length=100)
@@ -17,6 +27,17 @@ class Product(BaseModel):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
     price = models.IntegerField()
     product_description = models.TextField()
+
+
+    def save(self, *args, **kwargs):
+
+        self.slug_field = slugify(self.product_name)
+
+        super(Product, self).save(*args, **kwargs)
+
+    
+    def __str__(self) -> str:
+        return self.product_name
 
 
 
