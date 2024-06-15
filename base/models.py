2129dee25c35bfe27ced9761cb41221392a58ef2
harshjainsk <harshjainsk531@gmail.com>
2024-06-15 12:23:47 +0530

    project setup done

--- base/models.py
+++ base/models.py
@@ -5,8 +5,8 @@ import uuid
 class BaseModel(models.Model):
 
     unique_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_add = True) 
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add = True) 
 
 
     class Meta:
