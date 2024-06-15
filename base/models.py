20bb261be059f09bad6e304e417d0b6ced523869
harshjainsk <harshjainsk531@gmail.com>
2024-06-15 11:24:41 +0530

    project initialised

---
+++ base/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+import uuid
+
+
+class BaseModel(models.Model):
+
+    unique_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_add = True) 
+
+
+    class Meta:
+
+        abstract = True
