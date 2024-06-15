6e948e85c907596833921df150dbf1255fcaa568
harshjainsk <harshjainsk531@gmail.com>
2024-06-15 23:26:10 +0530

    added code for product and home page

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -21,8 +21,10 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+    path("", include("home.urls")),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
+    path("product/", include("products.urls")),
 ]
 
 
